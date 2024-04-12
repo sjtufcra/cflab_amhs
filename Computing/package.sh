5a86e5452afd37bd35d2d1b82c969b5a5b19e69d
Daniel <danielghost@live.com>
2024-04-12 11:58:57 +0800

    Remove uppercase 'Computing' folder

--- Computing/package.sh
+++
@@ -1,3 +0,0 @@
-echo "pak gen sucess, please install amhs_sjtu"
-python setup.py build
-python setup.py sdist
--- Computing/server/graph/config.py
+++
@@ -1,7 +0,0 @@
-httpServer = dict(uname ='0.0.0.0',
-    paths = "src.api:app",
-    ports = 8055,
-    log = 'info',
-    reloade =True)
-
-config_file_path= 'cflab_amhs/config.yaml'
