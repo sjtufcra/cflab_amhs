b1c87906a5add274dad281a4be07198068f50b5e
Daniel <danielghost@live.com>
2024-05-21 18:54:04 +0800

    fix: bug

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ else
     echo "amhs_sjtu not installed"
 fi
 echo "Install amhs_sjtu"
-pip3 install --user computing/dist/amhs_sjtu-1.0.0.tar.gz
+pip3 install --user computing/dist/amhs_sjtu-1.0.1.tar.gz
 pip3 install -r requirements.txt
 echo "Install amhs_sjtu success"
