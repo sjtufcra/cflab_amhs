44f801ffb2f2283343bb7dfb4412ffa2c0eaf92a
Daniel <danielghost@live.com>
2024-05-21 17:50:09 +0800

    fix: bug

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-pip3 uninstall amhs_sjtu 
-pip3 install --user computing/dist/amhs_sjtu-1.0.0.tar.gz 
+#!/bin/bash
+if pip3 show amhs_sjtu >/dev/null 2>&1;then
+    echo "Uninstall amhs_sjtu"
+    pipe uninstall amhs_sjtu -y
+else
+    echo "amhs_sjtu not installed"
+fi
+echo "Install amhs_sjtu"
+pip3 install --user computing/dist/amhs_sjtu-1.0.0.tar.gz
+
+
+# # old
+# pip3 uninstall amhs_sjtu -y
+# pip3 install --user computing/dist/amhs_sjtu-1.0.0.tar.gz 
